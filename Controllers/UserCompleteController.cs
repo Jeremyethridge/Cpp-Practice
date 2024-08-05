1c244885b8a0facd7fac3dcc51a6bf482b52f229
JeremyEthridge <Jeremyethridge6@yahoo.com>
2024-08-04 22:17:04 -0400

    Finished up the Stored Procedures and SQL section of the class and will be adding the Stored Procedures to my application

---
+++ Controllers/UserCompleteController.cs
@@ -0,0 +1,99 @@
+using System.Data;
+using DotnetAPI.Data;
+using DotnetAPI.Dtos;
+using DotnetAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetAPI.Controllers;
+[ApiController]
+[Route("[controller]")]
+public class UserCompleteController : ControllerBase
+{
+    DataContextDapper _dapper;
+    public UserCompleteController(IConfiguration config)
+    {
+        _dapper = new DataContextDapper(config);
+    }
+
+    [HttpGet("GetUsers{userId}")]
+    // public IActionResult Test()
+    public IEnumerable<UserComplete> GetUsers(int userId)
+    {
+        string sql = @"
+        EXEC TutorialAppSchema.spUsers_Get @UserId =" + userId.ToString();
+        IEnumerable<UserComplete> users = _dapper.LoadData<UserComplete>(sql);
+        return users;
+    }
+
+    [HttpGet("GetSingleUser/{userId}")]
+    // public IActionResult Test()
+    public User GetSingleUser(int userId)
+    {
+        string sql = @"
+        SELECT [UserId]
+             ,[FirstName]
+             ,[LastName]
+            , [Email]
+            , [Gender]
+            , [Active]
+        FROM TutorialAppSchema.Users
+            WHERE UserId = " + userId.ToString();
+        User user = _dapper.LoadDataSingle<User>(sql);
+        return user;
+    }
+
+    [HttpPut("EditUser")]
+    public IActionResult EditUser(User user)
+    {
+        string sql = @"
+        UPDATE TutorialAppSchema.Users
+                SET [FirstName] = '" + user.FirstName +
+                 "', [LastName] = '" + user.LastName +
+                 "', [Email] =  '" + user.Email +
+                "', [Gender] =  '" + user.Gender +
+                 "', [Active] = '" + user.Active +
+                 "' WHERE UserId = " + user.UserId;
+        if (_dapper.ExecuteSql(sql))
+        {
+            return Ok();
+        }
+        throw new Exception("Failed to Update User");
+    }
+
+    [HttpPost("AddUser")]
+    public IActionResult AddUser(UserToAddDto user)
+    {
+        string sql = @"
+        INSERT INTO TutorialAppSchema.Users(
+            [FirstName],
+            [LastName],
+            [Email],
+            [Gender],
+            [Active]
+    ) VALUES (" +
+            "'" + user.FirstName +
+                "', '" + user.LastName +
+                "', '" + user.Email +
+                "', '" + user.Gender +
+                "', '" + user.Active +
+"')";
+        if (_dapper.ExecuteSql(sql))
+        {
+            return Ok();
+        }
+        throw new Exception("Failed to Add User");
+    }
+
+    [HttpDelete("DeleteUser/{userId}")]
+    public IActionResult DeleteUser(int userId)
+    {
+        string sql = @"DELETE FROM TutorialAppSchema.Users WHERE UserId = 
+        " + userId.ToString();
+        if (_dapper.ExecuteSql(sql))
+        {
+            return Ok();
+        }
+        throw new Exception("Failed to Delete User");
+    }
+}
+
--- obj/Debug/net8.0/DotnetAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotnetAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aaef00c28e0f853f1a22adbde1652520abab3723")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27738bead5cb81614d1393f5548e316c0e1ad683")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
